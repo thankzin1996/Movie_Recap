79e4975b370979368ddd471e95df0b40c4266c43
ThankZinTun <114830812+thankzin1996@users.noreply.github.com>
2026-06-27 17:10:19 +0630

    Configure app to run on dynamic port from environment
    Update app to use environment variable for port.

--- app.py
+++ app.py
@@ -35,5 +35,7 @@ def index():
     return render_template('index.html', recap=recap)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    # Railway က ပေးတဲ့ port ကို သုံးဖို့ OS ကနေ ယူရပါမယ်
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
